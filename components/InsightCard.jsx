a1e6f3552e5bb223c96b5fd020f427ef1f86d11c
nakjakwulfpak <nakul11999@gmail.com>
2023-09-08 19:00:02 +0530

    final-update

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -9,7 +9,7 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
     variant={fadeIn('up', 'spring', index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
   >
-    <img 
+    <img
       src={imgUrl}
       alt="planet-01"
       className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
@@ -19,6 +19,9 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
       <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
         <h4 className="font-normal lg:text-[42px] text-[26px] text-secondary-white">{title}</h4>
         <p className="mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">{subtitle}</p>
+      </div>
+      <div className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white">
+        <img src="arrow.svg" alt="arrow" className="w-[40%] h-[40%] object-contain" />
       </div>
     </div>
   </motion.div>
